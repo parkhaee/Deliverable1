c918fce8dbf2abd705ed7ebf875a6c093411f779
parkhaee <parkhaee@sheridancollege.ca>
2019-06-03 15:13:38 -0400

    added Card class code_hailey

--- src/project/Card.java
+++ src/project/Card.java
@@ -7,20 +7,55 @@ package project;
 
 /**
  * A class to be used as the base Card class for the project. Must be general
- * enough to be instantiated for any Card game. Students wishing to add to the code 
+ * enough to be instantiated for any Card game. Students wishing to add to the code
  * should remember to add themselves as a modifier.
+ *
  * @author megha, 2019
  */
-public abstract class Card 
+public abstract class Card
 {
-    //default modifier for child classes
-    
-    /**
-     * Students should implement this method for their specific children classes 
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
-    
-    @Override
-    public abstract String toString();
-    
+   //default modifier for child classes
+
+   /**
+    * Students should implement this method for their specific children classes
+    *
+    * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+    */
+   public enum Suit
+   {
+      HEARTS, SPADES, DIAMONDS, CLUBS
+   }
+   public enum Value
+   {
+      ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+   }
+
+   private final Suit suit;
+   private final Value value;
+
+   public Card (Suit s, Value v)
+   {
+      suit = s;
+      value = v;
+   }
+
+   /**
+    * @return the suit
+    */
+   public Suit getSuit ()
+   {
+      return suit;
+   }
+
+   /**
+    * @return the value
+    */
+   public Value getValue ()
+   {
+      return value;
+   }
+
+   @Override
+   public abstract String toString ();
+
 }
